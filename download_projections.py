c443067dae89d1d82ff8f185d69561f43d3b3c29
stigscousin <robsuttman@gmail.com>
2025-05-24 21:47:09 -0700

    Fix: Remove --user-data-dir for Heroku Chrome compatibility; add extra stability flags

--- download_projections.py
+++ download_projections.py
@@ -147,8 +147,12 @@ def download_projections():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--window-size=1920,1080')
-    user_data_dir = tempfile.mkdtemp()
-    chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
+    # user_data_dir = tempfile.mkdtemp()
+    # chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
     
     # Set download directory
     download_dir = os.path.join(os.getcwd(), 'projections')
